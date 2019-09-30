aabff77cb7fe36025d8dba2f225faf2df0cd8fab
jimanPark <im@gman.io>
2019-09-30 23:27:55 +0900

    crypto hash

--- pages/api/file.tsx
+++ pages/api/file.tsx
@@ -1,16 +1,23 @@
 import crypto from 'crypto';
 import { NextApiRequest, NextApiResponse } from 'next';
 
+const iv = Buffer.alloc(16, 0);
+
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  const sha = crypto.createHash('sha1');
-  sha.update('한글을 입력');
-  const hash = sha.digest('hex');
-  console.log(hash);
-  if (req.method === 'GET') {
-    // Process your POST request
-  } else {
-    // Handle the rest of your HTTP methods
-  }
+  const key = crypto.scryptSync('password', 'salt', 24);
+  const cipher = crypto.createCipheriv('aes-192-cbc', key, iv);
+  const deCipher = crypto.createDecipheriv('aes-192-cbc', key, iv);
+  cipher.setAutoPadding(false);
+  deCipher.setAutoPadding(false);
+  cipher.update('dfsdfdsf', 'utf8', 'base64');
+
+  const encrypt = cipher.final('base64');
+  console.log(encrypt);
+
+  deCipher.update(encrypt, 'base64', 'utf8');
+  const decrypted = deCipher.final('utf8');
+  console.log(decrypted);
+
   res.setHeader('Content-Type', 'application/json');
   res.statusCode = 200;
   res.end(JSON.stringify({ service: 'ok' }));
