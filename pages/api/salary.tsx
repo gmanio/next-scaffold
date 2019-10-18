3e7c7e244606103329a86b1675d587f897b17bb2
jimanPark <im@gman.io>
2019-10-18 23:11:14 +0900

    added micro apollo

--- pages/api/salary.tsx
+++ pages/api/salary.tsx
@@ -1,10 +1,31 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { dbHelper } from '../middleware/dbHelper';
 
-export default dbHelper(async (req: NextApiRequest, res: NextApiResponse) => {
-  // console.log(req);
+import { ApolloServer, gql } from 'apollo-server-micro';
 
-  res.setHeader('Content-Type', 'application/json');
-  res.statusCode = 200;
-  res.end();
-});
+const typeDefs = gql`
+  type Query {
+    users: [User!]!
+  }
+  type User {
+    name: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    users (parent, args, context) {
+      return [{ name: 'Nextjs' }]
+    }
+  }
+}
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+export default apolloServer.createHandler({ path: '/api/salary' })
