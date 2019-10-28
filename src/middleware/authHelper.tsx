9daff258fc2bdfdc8d31aeaca0997c38227f8d58
jiman.park <jiman.park@woowahan.com>
2019-10-29 08:45:45 +0900

    added auth Helper

---
+++ src/middleware/authHelper.tsx
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import Cookies from 'cookies';
+
+type handleApiRequest = (req: NextApiRequest, res: NextApiResponse) => void;
+
+export const authHelper = (handleApiRequest: handleApiRequest) => (req: NextApiRequest, res: NextApiResponse) => {
+  const cookies = new Cookies(req, res);
+  console.log(cookies.get('tester'));
+  cookies.set('tester', 'dfsdfsd', { maxAge: 5000 });
+
+  if (req.method === 'GET') {
+  } else {
+  }
+  return handleApiRequest(req, res);
+};
