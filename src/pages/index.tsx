86ca861f76a9c3b11714c6411e8656dbaaa4b135
jimanPark <im@gman.io>
2019-11-21 21:24:04 +0900

    korean money translator

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import KakaoUser from '../models/KakaoUser';
+import { ChangeEvent } from 'react';
 
 export default class extends React.PureComponent {
   static async getInitialProps () {
     console.log('getInitialProps');
   }
 
+  public inputRef = React.createRef<HTMLInputElement>();
   state: {
     kakao_access_token: '',
     user: {}
@@ -47,6 +49,53 @@ export default class extends React.PureComponent {
     console.log(status);
   }
 
+  handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const stringNumbers = ['', '일', '이', '삼', '사', '오', '육', '칠', '팔', '구'];
+    const unitNumbers = ['', '십', '백', '천', '', '십', '백', '천', '', '십', '백', '천', '', '십', '백', '천'];
+    const data = e.currentTarget.value.split('');
+    let korean = '';
+
+    for (const [index, value] of data.reverse().entries()) {
+      const koreanNumber = stringNumbers[Number(value)];
+      console.log(index, value, stringNumbers[Number(value)]);
+      let concatString = '';
+      if (koreanNumber !== '') {
+        concatString = (koreanNumber + unitNumbers[index]);
+      }
+      if (index == 4) {
+        concatString += '만';
+      }
+      if (index == 8) {
+        concatString += '억';
+      }
+      if (index == 12) {
+        concatString += '조';
+      }
+      korean = concatString + korean;
+      // concatString += stringNumbers[Number(value)];
+    }
+
+    console.log(korean + '원');
+    // function numberWithCommas (x) {
+    //   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    // }
+
+    // const sNumber = numberWithCommas(data);
+    // const arrNumber = sNumber.split(',');
+    // console.log(arrNumber);
+    // for (const unit of arrNumber) {
+    //   const arrUnit = unit.split('');
+    //   for (const number of arrUnit) {
+    //
+    //   }
+    // }
+
+    // 10,000 일십만원
+
+    console.log(e);
+    console.log(e.currentTarget.value);
+  }
+
   render () {
     return (
       <>
@@ -59,6 +108,8 @@ export default class extends React.PureComponent {
           <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_SM.gif" name="submit" alt="PayPal - The safer, easier way to pay online!"/>
           <img alt="" src="https://www.paypalobjects.com/ko_KR/i/scr/pixel.gif" width="1" height="1"/>
         </form>
+
+        <input ref={this.inputRef} defaultValue={'1000'} onChange={this.handleInput}/>
       </>
     )
   }
